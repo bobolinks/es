ae8a851e99461521f624652c7775eb003ebea5d5
Eileen <xiaolan8318@163.com>
2019-04-20 17:06:52 +0800

    add sample render

---
+++ samples/render/default.js
@@ -0,0 +1,17 @@
+$es.router.define({
+    id: '/',
+    template: `
+    <div>
+        <table>
+            <tr align=center style='background:lightgray'>
+                <td style='width:50%'><button onclick='window.tstick = Date.now(); $es.it("idslow").data.title="Title[changed]";'>slow render</button><td>
+                <td style='width:50%'><button onclick='window.tstick = Date.now(); $es.it("idfast").data.title="Title[changed]";'>fast render</button><td>
+            </tr>
+            <tr style='height:500'>
+                <td><es-use id='idslow' component='slow'></es-use><td>
+                <td><es-use id='idfast' component='fast'></es-use><td>
+            </tr>
+        </table>
+    </div>
+    `
+});
